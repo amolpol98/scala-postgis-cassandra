4c0a5835f00a4d4d8a0b8e6e9f3494846577f78a
Amol Pol <amol.pol@here.com>
2018-08-05 19:41:30 +0530

    added logging capability in main class

--- src/main/scala/com/assignment/Main.scala
+++ src/main/scala/com/assignment/Main.scala
@@ -1,11 +1,13 @@
 package com.assignment
 
 import akka.actor.ActorSystem
+import akka.event.LoggingAdapter
 import akka.stream.ActorMaterializer
 import com.typesafe.config.ConfigFactory
 import akka.http.scaladsl.Http
+
 import scala.concurrent.ExecutionContext
-import scala.util.{ Failure, Success }
+import scala.util.{Failure, Success}
 import routes.Routes
 
 object Main extends App with Routes {
@@ -14,11 +16,14 @@ object Main extends App with Routes {
   implicit val materializer: ActorMaterializer = ActorMaterializer()
   implicit val executionContext: ExecutionContext = system.dispatcher
   val settings = Settings(system)
+
+  val log: LoggingAdapter = system.log
+
   val bindingFuture = Http().bindAndHandle(routes, settings.server.host, settings.server.port).onComplete {
     case Success(binding) =>
-      println(s"Ready at ${binding.localAddress}")
+      log.info(s"Ready at ${binding.localAddress}")
     case Failure(ex) =>
-      println(s"Failed on startup: ${ex.getMessage}")
+      log.error(s"Failed on startup: ${ex.getMessage}")
       system.terminate()
   }
 }
